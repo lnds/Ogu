7045973623a2f3ebf0f15b34986c3ec08519bf9c
Eduardo Díaz <ediaz@dcc8090.cl>
2020-11-05 23:53:10 -0300

    add banner

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,13 +1,14 @@
 use std::path::PathBuf;
-use std::io::Result as IOResult;
+use std::io::{Result as IOResult, Error, ErrorKind};
 use structopt::StructOpt;
+use figlet_rs::FIGfont;
 
-const HELP_GREETINGS :&str = "\n\t\tOLA AMIKO MIO DE MI\n\n";
+const HELP_GREETINGS: &str = "\n\t\tOLA AMIKO MIO DE MI\n\n";
+const VERSION: &str = "Ogu compiler version 0.3.0 (Ñeclito)";
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "ogu", about = HELP_GREETINGS)]
 pub struct Params {
-
     /// shows akarrú banner
     #[structopt(short, long)]
     banner: bool,
@@ -16,14 +17,27 @@ pub struct Params {
     #[structopt(short, long)]
     print: bool,
 
-    #[structopt(parse(from_os_str), help="ogu modules...")]
+    #[structopt(parse(from_os_str), help = "ogu modules...")]
     files: Vec<PathBuf>,
 
-    #[structopt(name="ARGS", required_if("has_args", "true"), last=true, help="args for main module")]
+    #[structopt(name = "ARGS", required_if("has_args", "true"), last = true, help = "args for main module")]
     args: Vec<String>,
 }
 
 pub fn run(params: Params) -> IOResult<()> {
-    println!("run {:?}", params);
+    if params.banner {
+        akarru()
+    } else {
+        println!("run {:?}", params);
+        Ok(())
+    }
+}
+
+
+fn akarru() -> IOResult<()> {
+    let standard_font = FIGfont::standand().map_err(|s| Error::new(ErrorKind::Other, s))?;
+    let figure = standard_font.convert("Akarrú").ok_or_else(|| Error::new(ErrorKind::Other, "no banner"))?;
+    println!("{}", figure);
+    println!("{}", VERSION);
     Ok(())
 }

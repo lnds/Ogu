94291cd6be3b20984f29cc53c07b9df7c02c1f97
Eduardo Díaz <ediaz@dcc8090.cl>
2020-12-31 10:42:22 -0300

    add traits type

--- ogu-lang/src/backend/modules/types/mod.rs
+++ ogu-lang/src/backend/modules/types/mod.rs
@@ -1,2 +1,3 @@
 pub(crate) mod basic_type;
 pub(crate) mod func_type;
+pub(crate) mod trait_type;
---
+++ ogu-lang/src/backend/modules/types/trait_type.rs
@@ -0,0 +1,28 @@
+use crate::backend::scopes::types::Type;
+
+#[derive(Clone, Debug)]
+pub(crate) struct TraitType {
+    name: String
+}
+
+impl TraitType {
+    pub(crate) fn new(name: &str) -> Box<dyn Type> {
+        Box::new(TraitType { name: name.to_string() })
+    }
+}
+
+impl Type for TraitType {
+    fn get_name(&self) -> String {
+       self.name.to_string()
+    }
+
+    fn get_signature(&self) -> String {
+        format!("Trait {}", self.get_name())
+    }
+}
+
+impl PartialEq for TraitType {
+    fn eq(&self, other: &Self) -> bool {
+        self.get_name() == other.get_name()
+    }
+}

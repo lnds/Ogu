f10b947d921624e1fe400b2da6260e6d27fe4404
Eduardo Díaz <ediaz@dcc8090.cl>
2021-01-24 12:21:36 -0300

    fix test on euler 1

--- ogu-lang/src/backend/modules/tests/test_eulers.rs
+++ ogu-lang/src/backend/modules/tests/test_eulers.rs
@@ -3,7 +3,7 @@ use crate::backend::modules::tests::make_module;
 use crate::backend::modules::types::basic_type::BasicType;
 use crate::backend::modules::types::func_type::FuncType;
 use crate::backend::modules::types::list_type::ListType;
-use crate::backend::modules::types::trait_type::TRAIT_UNKNOWN;
+use crate::backend::modules::types::trait_type::{TRAIT_UNKNOWN, TRAIT_NUM};
 use crate::backend::scopes::types::TypeClone;
 use indoc::indoc;
 
@@ -18,24 +18,28 @@ fn test_euler_1() {
             union [] ys = ys
             union (x :: xs) ys = x :: union xs ys
 
-            sum [] = 0
+            sum [] = 0N
             sum x :: xs = x + sum xs
 
-            main () =
-                union [3, 6 .. 999] [5, 10 .. 999] |> sum |> println!
-             "#},
+            result = union [3, 6 .. 999] [5, 10 .. 999] |> sum"#},
         default_sym_table(),
     );
-    println!("module = {:?}", module);
+    //println!("module = {:?}", module);
     assert!(module.is_ok());
     let module = module.unwrap();
     let decls = module.get_decls();
-    println!("TEST DECLS = {:#?}", decls);
+    //println!("TEST DECLS = {:#?}", decls);
     assert_eq!(
         decls[0].get_type(),
         FuncType::new_opt(
             Some(vec![ListType::new_list(TRAIT_UNKNOWN.clone_box()), ListType::new_list(TRAIT_UNKNOWN.clone_box())]),
             ListType::new_list(TRAIT_UNKNOWN.clone_box()))
     );
-
+    assert_eq!(
+        decls[1].get_type(),
+        FuncType::new_opt(
+            Some(vec![ListType::new_list(TRAIT_NUM.clone_box())]),
+           TRAIT_NUM.clone_box())
+    );
+    assert_eq!(decls[2].get_type(), Some(BasicType::int()));
 }
